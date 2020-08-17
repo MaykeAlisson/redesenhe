3ce5ce8667bb036ab3c749e02b4ef4b4d94f381c
Mayke Alisson <maykealison@gmail.com>
2020-08-17 10:01:49 -0300

    init

---
+++ src/test/java/br/com/redesenhe/resource/NativeRedesenheResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.redesenhe.resource;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeRedesenheResourceIT extends RedesenheResourceTest {
+
+    // Execute the same tests but in native mode.
+}
